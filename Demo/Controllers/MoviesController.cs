322d53fd77192bd30ca0c46feaa6d84fef0c2212
mohamed_hamdi_mahmoud <01124833532mo>
2024-09-06 20:07:06 +0300

    Part03 Action ReturnTypes

--- Demo/Controllers/MoviesController.cs
+++ Demo/Controllers/MoviesController.cs
@@ -5,8 +5,16 @@ namespace Demo.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly IConfiguration _configuration;
+
         // action : public non static object member methode inside the controller 
         //baseurl/movies/getmovie/{id}
+
+        public MoviesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public string Index()
         {
@@ -14,10 +22,36 @@ namespace Demo.Controllers
         }
         [HttpGet]
 
-        public string GetMovie(int id)
+        public IActionResult GetMovie(int id)
         {
-            return $"movie with id : {id}";
-        } 
+            if (id == 0)
+            {
+                //return bad request
+                return BadRequest();
+            }
+            if (id == 100)
+            {
+                // return notfindresult()
+                return NotFound();
+            }
+            //ContentResult result = new ContentResult();
+            //result.Content = $"<h1> movie with id :{id} </h1>";
+            //result.ContentType = "text/html";
+            //result.StatusCode = 200;
+
+
+            return Content($"<h1> movie with id :{id} </h1>", "text/html");
+
+        }
+
+        public IActionResult hamada()
+        {
+            RedirectResult result = new RedirectResult(_configuration["Wazuf"] ?? string.Empty);
+            RedirectResult redirect = new RedirectResult($"{_configuration["BaseURL"]}/Movies/GetMovie/10");
+            RedirectToActionResult redirectToActionResult = new RedirectToActionResult(nameof(GetMovie), "Movies", new { id = 10 });
+            
+                        return result;
+        }
 
         [HttpGet]
         //[AcceptVerbs("Get","Post")]
